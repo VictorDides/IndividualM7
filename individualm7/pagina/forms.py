376e26362e3487c894300708f0f2f00dcba5454a
VictorDides <vadides@gmail.com>
2023-08-18 19:12:34 -0400

    individual 5

--- individualm7/pagina/forms.py
+++ individualm7/pagina/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Tarea
+from .models import Tarea, Etiqueta
 
 class TareaForm(forms.ModelForm):
     class Meta:
         model = Tarea
         fields = ['titulo', 'descripcion', 'fecha_vencimiento', 'etiquetas']
+
+
+class FiltroTareasForm(forms.Form):
+    etiquetas = forms.ModelMultipleChoiceField(queryset=Etiqueta.objects.all(), required=False)
+    completada = forms.BooleanField(required=False)
--- individualm7/pagina/views.py
+++ individualm7/pagina/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tarea
-from .forms import TareaForm 
+from .forms import TareaForm, FiltroTareasForm
 
 # Create your views here.
 def landing(request):
     return render(request,'pagina/landing.html')
 
-def listar_tareas(request):
+def listar_tareas_old(request):
     tareas = Tarea.objects.filter().order_by('fecha_vencimiento')
     return render(request, 'pagina/listar_tareas.html', {'tareas': tareas})
 
@@ -25,6 +25,23 @@ def crear_tarea(request):
         form = TareaForm()
     
     return render(request, 'pagina/crear_tarea.html', {'form': form})
+
+def listar_tareas(request):
+    filtro_form = FiltroTareasForm(request.GET)
+    tareas = Tarea.objects.all()
+
+    if filtro_form.is_valid():
+        etiquetas_seleccionadas = filtro_form.cleaned_data.get('etiquetas')
+        completada = filtro_form.cleaned_data.get('completada')
+
+        if etiquetas_seleccionadas:
+            tareas = tareas.filter(etiquetas__in=etiquetas_seleccionadas)
+        
+        if completada is not None:
+            tareas = tareas.filter(completada=completada)
+    
+    tareas = tareas.order_by('fecha_vencimiento')
+    return render(request, 'pagina/listar_tareas.html', {'tareas': tareas, 'filtro_form': filtro_form})
 
 
 def editar_tarea(request, tarea_id):
